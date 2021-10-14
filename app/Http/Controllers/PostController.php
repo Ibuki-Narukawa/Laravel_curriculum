dde3fcd2cde227ae31a3f817c8df434369397606
Ibuki-Narukawa <ibuki0212job@gmail.com>
2021-10-14 22:39:43 +0900

    blog_display

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,23 @@ use App\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post){
-    return $post->get();;   
+    public function index(){
+        $data = [
+            ['title'=>'title01', 'body'=>'This is a sample body01.'],
+            ['title'=>'title02', 'body'=>'This is a sample body02.'],
+            ['title'=>'title03', 'body'=>'This is a sample body03.'],
+            ['title'=>'title04', 'body'=>'This is a sample body04.'],
+            ['title'=>'title05', 'body'=>'This is a sample body05.'],
+            ['title'=>'title06', 'body'=>'This is a sample body06.'],
+            ['title'=>'title07', 'body'=>'This is a sample body07.'],
+            ['title'=>'title08', 'body'=>'This is a sample body08.'],
+            ['title'=>'title09', 'body'=>'This is a sample body09.'],
+            ['title'=>'title10', 'body'=>'This is a sample body10.']
+        ];
+        
+        return view('posts.index', ['data'=>$data]);
+
     }
+    
+    
 }
